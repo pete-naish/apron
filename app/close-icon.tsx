bb73a34da35212b6fcfea499013affc95e8077ba
Pete Naish <peter.naish@framestore.com>
2024-04-26 16:36:31 +0100

    add basic markup and style according to figma design

---
+++ app/close-icon.tsx
@@ -0,0 +1,21 @@
+const CloseIcon = ({}) => {
+  return (
+    <svg
+      width="48"
+      height="48"
+      viewBox="0 0 48 48"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="24" cy="24" r="23" stroke="#D4D2C4" stroke-width="2" />
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M21.8788 24L13.4395 32.4393L15.5608 34.5606L24.0001 26.1213L32.4395 34.5606L34.5608 32.4393L26.1214 24L34.5608 15.5607L32.4395 13.4393L24.0001 21.8787L15.5608 13.4393L13.4395 15.5607L21.8788 24Z"
+        fill="#D4D2C4"
+      />
+    </svg>
+  );
+};
+
+export default CloseIcon;
